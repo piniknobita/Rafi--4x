d8029af6cad1211c7541018b845d07918d6ad549
Mohamed-kabir <162877423+Mohamed-kabir@users.noreply.github.com>
2024-03-27 12:04:26 +0600

    Update Admin2.js

--- scripts/commands/Admin2.js
+++ scripts/commands/Admin2.js
@@ -26,19 +26,22 @@ const time = process.uptime(),
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
-var callback = () => api.sendMessage({body:`
---------------------------------------------
-𝐍𝐚𝐦𝐞•—»✨𝐀𝐝𝐦𝐢𝐧 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧-!!✨
+var callback = () => api.sendMessage({body:`}•┄┅══❁≧◉≦𝐌𝐎𝐀𝐇𝐀𝐌𝐌𝐀𝐃"𝐊𝐀𝐁𝐈𝐑≧◉≦❁══┅┄• 
 
-༊_۵༎-𝐂-𝐄-𝐎 🩷⃝🌼 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝_𝐊𝐚𝐛𝐢𝐫<😻😽>] 🩷
+•—»✨𝐀𝐝𝐦𝐢𝐧 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧-!!✨
+
+༊_۵༎-𝐂-𝐄-𝐎 🩷⃝🌼 𝐊𝐀𝐁𝐈𝐑<😻😽>] 🩷
 
 𝐂𝐀𝐋𝐋 𝐌𝐄:𒁍 ⃝𓆩𝐌𝐀𝐅𝐈𝐘𝐀𓆪⸙🥷
 
 •┄┅══❁𝐂𝐨𝐧𝐭𝐚𝐜𝐭❁══┅┄• 
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐈𝐝: https://www.facebook.com/profile.php?id=100084055394893
-
+𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 :https://www.facebook.com/profile.php?id=100084055394893
+𝐓𝐄𝐋𝐈𝐆𝐑𝐀𝐌 :<𝐑𝐀𝐅𝐈1231
 •—»✨𝐎𝐭𝐡𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨«—•
 
+𝐀𝐃𝐑𝐄𝐒𝐒 : 𝐌𝐘𝐌𝐄𝐍𝐒𝐈𝐍𝐆 💜🍒
+
+𝐒𝐓𝐄𝐓𝐔𝐒 : 𝐒𝐈𝐍𝐆𝐄𝐋.𝐏𝐑𝐎.𝐌𝐀𝐗👿
 𝐓𝐔𝐏𝐄 / 𝐀𝐃𝐌𝐈𝐍 
 
 𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗 : [${global.config.PREFIX}]
@@ -47,7 +50,9 @@ var callback = () => api.sendMessage({body:`
 
 𝐁𝐎𝐓 𝐈𝐒 𝐑𝐔𝐍𝐍𝐈𝐍𝐆 ${hours}:${minutes}:${seconds}.
 
-𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐢𝐧𝐠  ༄🌺\n[ 🩷 ]${global.config.BOTNAME}`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐢𝐧𝐠  ༄🌺\n[ 🩷 ]${global.config.BOTNAME}
+
+•┄┅══❁≧◉≦𝐌𝐎𝐀𝐇𝐀𝐌𝐌𝐀𝐃"𝐊𝐀𝐁𝐈𝐑≧◉≦❁══┅┄• `,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://graph.facebook.com/100084055394893/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
